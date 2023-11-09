cf3eee66e49302bb5a87807ead025350c4494809
Jose Alfredo Sitanggang <josealfredositanggang@gmail.com>
2023-11-09 09:49:21 +0700

    error handling

--- converter.py
+++ converter.py
@@ -3,6 +3,12 @@ import json
 
 class LasConverter :
     def __init__(self,file):
+
+        ext = file.rsplit(".",1)[-1].lower() 
+        if  ext != "las":
+            raise Exception("File format no supported!")
+
+        self.supported_version = {2.0}
         self.__log = {"version":{},"well":{},"curve":{},"parameter":{},"data" :{},"other":""}
         self.__generated_keys = []
         self.__null_val = None
@@ -13,10 +19,13 @@ class LasConverter :
 
     def __convert(self):
         section = ""
+        rules = {"version", "well", "parameter", "curve"}
+
         for line in self.__lines:
             content = {}
 
             if len(line) <=1 and ord(line)== 10:
+                # line just enter or "\n"
                 continue
             
             if "#" in line :
@@ -26,12 +35,14 @@ class LasConverter :
                 section = self.__get_current_section(line)
                 continue
 
-            if section in {"version","well","parameter", "curve"}:
+            if section in rules:
 
+                # index of seperator
                 mnem_end = re.search("[.]{1}",line).end()
                 unit_end = mnem_end + re.search("[ ]{1}",line[mnem_end:]).end()
                 colon_end = unit_end + re.search("[:]{1}",line[unit_end:]).start()
 
+                # divide line
                 mnem = line[:mnem_end-1].strip()
                 unit = line[mnem_end:unit_end].strip()
                 data = line[unit_end:colon_end].strip()
@@ -40,18 +51,19 @@ class LasConverter :
 
                 if len(data) == 0:
                     data = None
+
                 if section == "well" and mnem == "NULL" :
                     self.__null_val = data
                     data = None
 
                 if section == "curve" :
-                    key = "api code"  
+                    key = "api code"
+
                 if data is not None :
                     if data == "NO" :
                         data = False
                     elif data == "YES":
                         data = True
-                    
                     else :
                         data = self.__parse(data)
 
@@ -60,6 +72,7 @@ class LasConverter :
                     "unit"  : unit,
                     "desc": desc
                 }
+
                 self.__log[section][mnem] = content
 
             elif section == "data":
@@ -74,14 +87,18 @@ class LasConverter :
 
         self.__has_converted = True
 
-
-
+    def get_las_version(self):
+        return self.__log["version"]["VERS"]["value"]
+            
     def __get_current_section(self,line):
 
         if '~V' in line :
             return 'version'
-            
-        elif '~W' in line :
+
+        if self.get_las_version() not in self.supported_version:
+            raise Exception("Version not supported!")
+
+        if '~W' in line :
             return 'well'
 
         elif '~C' in line:
@@ -91,16 +108,25 @@ class LasConverter :
             return 'parameter'
 
         elif '~A' in line:
+            # generate keys of log[data] based on log[curve]
             self.__generated_keys = [e.lower() for e in self.__log['curve'].keys()]
             for key in self.__generated_keys:
                 #inital all key to empty list
                 self.__log['data'][key]=[]
-
             return 'data'
-
-
         else :
             return 'others'
+
+    def __parse(self, x):
+        try:
+            x = int(x)
+        except ValueError:
+            try:
+                x = float(x)
+            except ValueError:
+                pass
+
+        return x
 
     def get_json(self,out="outfile"):
         out = out +".json"
@@ -117,21 +143,9 @@ class LasConverter :
             self.__convert()
         return self.__log
     
-    def __parse(self,x):
-        try:
-            x = int(x)
-        except ValueError:
-            try:
-                x = float(x)
-            except ValueError:
-                pass
-
-        return x
-
-
 
 if __name__ == "__main__":
-    c = LasConverter("files/sample1.las")
+    c = LasConverter("files/sample0.las")
     json_files = c.get_json()
 
 
